04f9118c2742fdf6603bc240c32adb30c927daf6
aster <137767097+aster-void@users.noreply.github.com>
2025-02-15 14:33:02 +0900

    server/cors: init

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,14 @@
 import { Hono } from "hono";
 import hello from "./routes/hello";
+import { cors } from "hono/cors";
 
-const app = new Hono().route("/", hello);
+const app = new Hono()
+	.use(
+		cors({
+			origin: [process.env.CORS_ALLOW_ORIGINS],
+		}),
+	)
+	.route("/", hello);
 
 export default app;
 export type App = typeof app;
