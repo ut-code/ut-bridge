7f681a838d4414fab118d32ca7d8129f4e3e420e
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2025-02-15 14:46:48 +0900

    install biome and lefthook (#10)
    * doc: add required packages for README
    
    * init: init root package, lefthook.yml, and biome.jsonc
    
    * config: configure biome and lefthook
    
    * biome: apply format and linting
    
    * init: github action
    
    * revert: changes in gitignore
    
    * lefthook: limit file types that are passed to biome
    
    * bun: run lefthook install after bun install

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { assert } from "./lib/panic.ts";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+const root = assert(document.getElementById("root"), "root not found");
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
