6af96a75b02894836c879b3b2b9126cf5dfba1f0
aster <137767097+aster-void@users.noreply.github.com>
2025-02-15 14:29:06 +0900

    biome: apply format and linting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,43 +1,43 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
-import type { App as Server } from "server";
 import { hc } from "hono/client";
+import type { App as Server } from "server";
 
 const client = hc<Server>("http://localhost:3000");
 
 function App() {
-	const [count, setCount] = useState(0);
-	client.index
-		.$get()
-		.then((it) => it.text())
-		.then((it) => console.log(it));
+  const [count, setCount] = useState(0);
+  client.index
+    .$get()
+    .then((it) => it.text())
+    .then((it) => console.log(it));
 
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>React + Vite</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React + Vite</h1>
+      <div className="card">
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
 }
 
 export default App;
