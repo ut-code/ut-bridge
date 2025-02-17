69f404b2e5d5af091537af67e5115cfd6a0a2225
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-02-17 16:12:53 +0900

    add: users crud

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,11 +1,51 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client"
+
+const prisma = PrismaClient();
 
 const router = new Hono();
 
-router.get("/", (c) => {});
+router.get("/", async (c) => {
+  const users = await prisma.user.findMany()
 
-router.post("/", (c) => {});
-router.put("/", (c) => {});
-router.delete("/", (c) => {});
+  return c.json(users)
+})
 
-export default router
+router.get("/:id", async(c) => {
+  const id = c.req.param('id')
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id
+    }
+  })
+  return c.json(user)
+});
+
+router.post("/", async (c) => {
+  const body = await c.req.json
+  const newUser = await prisma.user.create({
+    data: body
+  })
+  return c.json(newUser)
+});
+
+router.put("/", async (c) => {
+  const updateContent = await c.req.json()
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: updateContent.id
+    },
+    data: updateContent
+  })
+  return c.json(updatedUser)
+});
+
+router.delete("/", async (c) => {
+  const body = await c.req.json()
+  const deletedUser = await prisma.user.delete({
+    where: { id: body.id }
+  })
+  return c.json(deletedUser)
+});
+
+export default router;
