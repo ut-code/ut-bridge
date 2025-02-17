81d1aea2b0fc8d33bf209029b6fdb6071aecd826
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-02-17 17:37:00 +0900

    add: zod schema

---
+++ server/zod/schema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const GenderEnum = z.enum(["male", "female", "other"])
+
+const DisplayLanguage = z.enum(["japanese", "english"])
+
+export const HobbySchema = z
+  .string()
+  // .min(1, { message: "趣味は1文字以上です" })
+  .max(25, { message: "趣味は25文字以下です" });
+export const IntroductionSchema = z
+  .string()
+  // .min(2, { message: "コメントは2文字以上です" })
+  .max(225, { message: "コメントは225文字以下です" });
+
+export const UserSchema = z.object({
+  id: z.string().uuid(),
+  guid: z.string(),
+  imageUrl: z.string(),
+  name: z.string(),
+  gender: GenderEnum.optional(),
+  isForeignStudent: z.boolean(),
+  DisplayLanguage: DisplayLanguage.optional(),
+  campusId: z.string(),
+  grade: z.number(),
+  hobby: HobbySchema,
+  introduction: IntroductionSchema
+})
