8ce56960b338af46658e739b19e1174ebad029ce
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-02-17 16:30:16 +0900

    delete: hello.ts

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
 import cors from "./middlewares/cors";
-import hello from "./routes/hello";
+import usersRoutes from "./routes/users";
 
-const app = new Hono().use(cors("CORS_ALLOW_ORIGINS")).route("/", hello);
+const app = new Hono().use(cors("CORS_ALLOW_ORIGINS"))
+
+app.get("/", (c) => c.text("Hello from Hono 🔥"));
+
+app.route("/users", usersRoutes);
 
 export default app;
 export type App = typeof app;
