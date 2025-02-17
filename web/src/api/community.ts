d8e01b489bfd93119be2daab7c8c112aa03b6b11
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-02-17 17:43:18 +0900

    ブラウザでユーザー一覧を見れるようになった

--- web/src/api/community.ts
+++
@@ -1,4 +0,0 @@
-import { client } from "../client";
-
-const res = await client.community.$get();
-export const data = res.json();
--- web/src/client.ts
+++ web/src/client.ts
@@ -1,5 +1,6 @@
 import { hc } from "hono/client";
 import type { App } from "server";
-import { env } from "./lib/env";
 
-export const client = hc<App>(env("NEXT_PUBLIC_API_ENDPOINT"));
+export const client = hc<App>(
+  process.env.NEXT_PUBLIC_API_ENDPOINT || "default_api_endpoint",
+);
