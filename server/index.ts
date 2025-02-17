a52c5b9ef1e33e0d3acdbb931087bf0073155a0d
Hiroki Oya <172092787+hiroki078@users.noreply.github.com>
2025-02-17 16:51:09 +0900

    add users routes (#34)
    * add users routes
    
    * fix
    
    * add: users crud
    
    * delete: hello.ts
    
    * fix: path parameter
    
    * fix

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,13 @@
 import { Hono } from "hono";
 import cors from "./middlewares/cors";
-import hello from "./routes/hello";
+import usersRoutes from "./routes/users";
 
-const app = new Hono().use(cors("CORS_ALLOW_ORIGINS")).route("/", hello);
+const app = new Hono()
+  .use(cors("CORS_ALLOW_ORIGINS"))
+
+  .get("/", (c) => c.text("Hello from Hono 🔥"))
+
+  .route("/users", usersRoutes);
 
 export default app;
 export type App = typeof app;
--- server/routes/hello.ts
+++
@@ -1,5 +0,0 @@
-import { Hono } from "hono";
-
-const route = new Hono().get("/", (c) => c.text("Hello from Hono 🔥"));
-
-export default route;
