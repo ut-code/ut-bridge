c94fe928c8bd94a0e196078d722dee4a72597ae3
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-02-18 19:07:45 +0900

    headerの位置を調整

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,10 +1,20 @@
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Header />
-      <div className="h-full">{children}</div>
-    </>
+    <div className="h-full">
+      <header className="h-16 w-full justify-center bg-primary fixed top-0 m-0">
+        <button type="button" className="text-white">
+          コミュニティ
+        </button>
+        <button type="button" className="text-white">
+          チャット
+        </button>
+        <button type="button" className="text-white">
+          設定
+        </button>
+      </header>
+      <div className="h-full overflow-y-auto pt-16">{children}</div>
+    </div>
   );
 }
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="h-16 w-full justify-center bg-primary sticky">
+    <header className="h-16 w-full justify-center bg-primary fixed top-0 m-0">
       <p className="text-white">コミュニティ チャット 設定</p>
     </header>
   );
