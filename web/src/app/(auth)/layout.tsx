24b6b48c42f67a5a0dab5f931c6f2a0fcc59e9ab
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-02-18 20:47:15 +0900

    headerにアイコンを追加

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,19 +1,11 @@
 // import Header from "@/components/Header";
 
+import Header from "@/components/Header";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
-      <header className="h-16 w-full justify-center bg-primary fixed top-0 m-0">
-        <button type="button" className="text-white">
-          コミュニティ
-        </button>
-        <button type="button" className="text-white">
-          チャット
-        </button>
-        <button type="button" className="text-white">
-          設定
-        </button>
-      </header>
+      <Header />
       <div className="h-full overflow-y-auto pt-16">{children}</div>
     </div>
   );
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+import { AppIcon } from "./AppIcon";
+
 export default function Header() {
   return (
     <header className="h-16 w-full justify-center bg-primary fixed top-0 m-0">
-      <p className="text-white">コミュニティ チャット 設定</p>
+      <Link
+        href="/community"
+        passHref
+        className="-translate-y-1/2 absolute top-1/2 left-3 transform"
+      >
+        <AppIcon width={30} height={30} />
+      </Link>
+      <button type="button" className="text-white">
+        コミュニティ
+      </button>
+      <button type="button" className="text-white">
+        チャット
+      </button>
+      <button type="button" className="text-white">
+        設定
+      </button>
     </header>
   );
 }
