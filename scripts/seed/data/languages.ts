38b0ccbdc2d8639f3e88d49b9ac662dafd2bd989
aster <137767097+aster-void@users.noreply.github.com>
2025-02-19 22:42:24 +0900

    feat: seed data

---
+++ scripts/seed/data/languages.ts
@@ -0,0 +1,18 @@
+export type Language = {
+  in_ja: string;
+  in_en: string;
+};
+
+export const languages: Language[] = [
+  { in_ja: "日本語", in_en: "Japanese" },
+  { in_ja: "英語", in_en: "English" },
+  { in_ja: "中国語", in_en: "Mandarin Chinese" },
+  { in_ja: "スペイン語", in_en: "Spanish" },
+  { in_ja: "ヒンディー語", in_en: "Hindi" },
+  { in_ja: "アラビア語", in_en: "Arabic" },
+  { in_ja: "フランス語", in_en: "French" },
+  { in_ja: "ロシア語", in_en: "Russian" },
+  { in_ja: "ポルトガル語", in_en: "Portuguese" },
+  { in_ja: "ベンガル語", in_en: "Bengali" },
+  { in_ja: "ドイツ語", in_en: "German" },
+];
--- scripts/seed/index.ts
+++ scripts/seed/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+import { languages } from "./data/languages.ts";
+import { universities } from "./data/universities.ts";
+
+import { prisma } from "../../server/config/prisma.ts";
+
+if (
+  await prisma.university.findUnique({ where: { name: universities[0].name } })
+) {
+  throw new Error("cannot run seed: university already exists");
+}
+
+for (const lang of languages) {
+  await prisma.language.create({
+    data: { name: lang.in_ja }, // TODO: make language accept more than one name
+  });
+}
+
+for (const univ of universities) {
+  const created = await prisma.university.create({
+    data: { name: univ.name },
+  });
+  for (const campus of univ.campuses) {
+    await prisma.campus.create({
+      data: {
+        name: campus.name,
+        universityId: created.id,
+      },
+    });
+  }
+  for (const div of univ.divisions) {
+    await prisma.division.create({
+      data: { name: div.name, universityId: created.id },
+    });
+  }
+}
