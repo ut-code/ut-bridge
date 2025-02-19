b46be8db778f67621e7fbea185febd492ea0bf02
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-02-19 20:52:49 +0900

    ページ全体のレイアウトを作成 (#52)
    * add head tag
    
    * auth上にlayout.tsxを導入
    
    * headerを追加
    
    * headerの位置を調整
    
    * headerにアイコンを追加
    
    * root で bun watchができるように
    
    * ボタンで遷移可能に、文字を大きく
    
    * カーソルを当てると色が変化するように
    
    * 色合いを変更
    
    * pathに応じて、headerの色合いが変わるように
    
    * アイコンの大きさを調整
    
    * 画面全体の色を設定
    
    * 不要なコマンド、ページを削除
    
    * fix bun command

--- web/src/app/(auth)/community/layout.tsx
+++ web/src/app/(auth)/community/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <div className="h-full">{children}</div>;
 }
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { AppIcon } from "./AppIcon";
+
+export default function Header() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <header className="z-0 h-16 w-full bg-tBlue fixed top-0 flex items-center">
+      <Link href="/community" passHref className="px-4">
+        <AppIcon width={36} height={36} />
+      </Link>
+      <Link
+        href="/community"
+        className="text-white text-2xl cursor-pointer px-4"
+      >
+        UT-Bridge
+      </Link>
+      <button
+        type="button"
+        className={`text-white text-xl px-4 h-full cursor-pointer transition-colors duration-200 ${
+          pathname === "/community" ? "bg-focus" : "hover:bg-focus"
+        }`}
+        onClick={() => {
+          router.push("/community");
+        }}
+      >
+        コミュニティ
+      </button>
+      <button
+        type="button"
+        className={`text-white text-xl px-4 h-full cursor-pointer transition-colors duration-200 ${
+          pathname === "/chat" ? "bg-focus" : "hover:bg-focus"
+        }`}
+        onClick={() => {
+          router.push("/chat");
+        }}
+      >
+        チャット
+      </button>
+      <button
+        type="button"
+        className={`text-white text-xl px-4 h-full cursor-pointer transition-colors duration-200 ${
+          pathname === "/settings" ? "bg-focus" : "hover:bg-focus"
+        }`}
+        onClick={() => {
+          router.push("/settings");
+        }}
+      >
+        設定
+      </button>
+    </header>
+  );
+}
