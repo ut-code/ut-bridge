68fdfd35bdbec6976ed438cd34f3b460bbbb2b4e
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-02-21 11:48:51 +0900

    ホーム画面にログイン画面へのボタンを設置

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <>Hello Next</>;
+  const router = useRouter();
+  return (
+    <>
+      Hello Next
+      <button
+        type="button"
+        onClick={() => {
+          router.push("/login");
+        }}
+        className="btn btn-primary"
+      >
+        ログイン画面へ
+      </button>
+    </>
+  );
 }
