d05b82a74b022621bfde1c3b05d2479da4d24406
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-02-27 15:51:26 +0900

    fix: biome

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -8,14 +8,19 @@ export default function Login() {
       <h1 className="text-3xl ">ログインページ</h1>
       <div className="flex justify-center items-center h-screen">
         <div className="text-center p-6 border rounded-lg shadow-md">
-          <p className="text-lg mt-10 mb-20">ECCSアカウントでのみ、ログイン可能です。</p>
+          <p className="text-lg mt-10 mb-20">
+            ECCSアカウントでのみ、ログイン可能です。
+          </p>
           <div className="my-10">
             <GoogleLoginButton />
           </div>
           <p className="text-sm text-gray-500 my-10">
-            末尾が@g.ecc.u-tokyo.ac.jpのECCSアカウントで認証してください<br />
+            末尾が@g.ecc.u-tokyo.ac.jpのECCSアカウントで認証してください
+            <br />
             {/* TODO: 遷移先を利用規約ページに変更する */}
-            <a href="/login" className="text-blue-500 underline">利用規約とプライバシーポリシーはこちら</a>
+            <a href="/login" className="text-blue-500 underline">
+              利用規約とプライバシーポリシーはこちら
+            </a>
           </p>
         </div>
       </div>
