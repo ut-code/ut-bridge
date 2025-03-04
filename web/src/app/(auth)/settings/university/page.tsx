619687b68b7551b8f16145f7f26769f4985807dc
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-04 16:56:25 +0900

    university info

--- web/src/app/(auth)/settings/university/page.tsx
+++ web/src/app/(auth)/settings/university/page.tsx
@@ -184,15 +184,15 @@ export default function Page() {
   };
 
   return (
-    <div className="mx-auto max-w-md p-4">
+    <div className="max-w my-20 p-4">
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        <label>
+        <label className="flex items-center justify-between">
           大学:
           <select
             name="universityId"
             onChange={handleChange}
             value={formData.universityId}
-            className="w-full border p-2"
+            className="my-4 w-1/2 rounded-xl border border-gray-500 bg-white p-2"
           >
             <option value="">大学を選択してください</option>
             {universities.map((univ) => (
@@ -203,13 +203,13 @@ export default function Page() {
           </select>
         </label>
 
-        <label>
+        <label className="flex items-center justify-between">
           学部:
           <select
             name="divisionId"
             value={formData.divisionId}
             onChange={handleChange}
-            className="w-full border p-2"
+            className="my-4 w-1/2 rounded-xl border border-gray-500 bg-white p-2"
             disabled={!divisions.length}
           >
             <option value="">学部を選択してください</option>
@@ -221,13 +221,13 @@ export default function Page() {
           </select>
         </label>
 
-        <label>
+        <label className="flex items-center justify-between">
           キャンパス:
           <select
             name="campusId"
             value={formData.campusId}
             onChange={handleChange}
-            className="w-full border p-2"
+            className="my-4 w-1/2 rounded-xl border border-gray-500 bg-white p-2"
             disabled={!campuses.length}
           >
             <option value="">キャンパスを選択してください</option>
@@ -239,9 +239,14 @@ export default function Page() {
           </select>
         </label>
 
-        <label>
+        <label className="flex items-center justify-between">
           学年:
-          <select name="grade" value={formData.grade} onChange={handleChange} className="w-full border p-2">
+          <select
+            name="grade"
+            value={formData.grade}
+            onChange={handleChange}
+            className="my-4 w-1/2 rounded-xl border border-gray-500 bg-white p-2"
+          >
             <option value="">学年を選択してください</option>
             <option value="B1">学部1年</option>
             <option value="B2">学部2年</option>
@@ -254,9 +259,15 @@ export default function Page() {
             <option value="D3">博士3年</option>
           </select>
         </label>
-        <button type="submit" className="rounded bg-blue-500 p-2 text-white" disabled={status === "loading"}>
-          {status === "loading" ? "登録中..." : "登録"}
-        </button>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="mt-15 w-50 rounded bg-blue-500 p-2 text-white"
+            disabled={status === "loading"}
+          >
+            {status === "loading" ? "登録中..." : "登録"}
+          </button>
+        </div>
       </form>
     </div>
   );
