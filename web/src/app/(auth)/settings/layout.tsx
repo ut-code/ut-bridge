e1bebafe9ced26fc51ca009911b3427774db5437
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-04 10:38:03 +0900

    fix: layout, /settings

--- web/src/app/(auth)/settings/layout.tsx
+++ web/src/app/(auth)/settings/layout.tsx
@@ -1,3 +1,10 @@
+import SideNav from "@/features/setting/SideNav";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <SideNav />
+      {children}
+    </>
+  );
 }
--- web/src/app/(auth)/settings/page.tsx
+++ web/src/app/(auth)/settings/page.tsx
@@ -1,85 +1,10 @@
 "use client";
 
-import LoginBadge from "@/features/auth/components/LoginBadge";
-import { useGoogleLogout } from "@/features/auth/functions/logout.ts";
 import { useUserContext } from "@/features/user/userProvider.tsx";
-import Image from "next/image";
-import Link from "next/link";
-import { formatUser } from "../../../features/format.ts";
 
 export default function Page() {
-  const { logout } = useGoogleLogout();
   const { me } = useUserContext();
 
   if (!me) return <div>User not found</div>;
-  const user = formatUser(me);
-  return (
-    <>
-      <Link href={"/settings/profile"} className="cursor-pointer px-4 text-2xl text-primary">
-        ユーザー編集画面へ
-      </Link>
-      設定画面
-      <div>
-        <Link href={"/settings/basic"}>基本情報</Link>
-        <Link href={"/settings/university"}>大学情報</Link>
-        <Link href={"/settings/language"}>言語情報</Link>
-        <Link href={"/settings/topic"}>トピック</Link>
-        <Link href={"#"}>友達</Link>
-        <Link href={"#"}>その他</Link>
-        <button type="button" className="btn btn-error m-5" onClick={logout}>
-          log out
-        </button>
-        <h1>自分のデータ</h1>
-        {user.imageUrl ? (
-          <Image
-            src={user.imageUrl}
-            alt={user.name ?? "User"}
-            width={48}
-            height={48}
-            className="h-12 w-12 rounded-full"
-          />
-        ) : (
-          <div className="flex h-12 w-12 items-center justify-center rounded-full bg-gray-300">aaa</div>
-        )}
-        <p>
-          <strong>Name:</strong> {user.name || "N/A"}
-        </p>
-        <p>
-          <strong>Gender:</strong> {user.gender || "N/A"}
-        </p>
-        <p>
-          <strong>Is Foreign Student:</strong> {user.isForeignStudent ? "Yes" : "No"}
-        </p>
-        <p>
-          <strong>Display Language:</strong> {user.displayLanguage}
-        </p>
-        <p>
-          <strong>Campus Name:</strong> {user.campus || "N/A"}
-        </p>
-        <p>
-          <strong>Grade:</strong> {user.grade ?? "N/A"}
-        </p>
-        <p>
-          <strong>Division:</strong> {user.division ?? "N/A"}
-        </p>
-        <p>
-          <strong>Mother Language:</strong> {user.motherLanguage ?? "N/A"}
-        </p>
-        <p>
-          <strong>Fluent Languages:</strong> {user.fluentLanguages.length > 0 ? user.fluentLanguages.join(", ") : "N/A"}
-        </p>
-        <p>
-          <strong>Learning Languages:</strong>{" "}
-          {user.learningLanguages.length > 0 ? user.learningLanguages.join(", ") : "N/A"}
-        </p>
-        <p>
-          <strong>Hobby:</strong> {user.hobby || "N/A"}
-        </p>
-        <p>
-          <strong>Introduction:</strong> {user.introduction || "N/A"}
-        </p>
-      </div>
-      <LoginBadge />
-    </>
-  );
+  return <></>;
 }
