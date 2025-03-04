666e68cd6d12b67fe0581f23e9c073f715f9c959
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-04 16:56:07 +0900

    topic info

--- web/src/app/(auth)/settings/topic/page.tsx
+++ web/src/app/(auth)/settings/topic/page.tsx
@@ -130,35 +130,41 @@ export default function Page() {
   };
 
   return (
-    <div className="mx-auto max-w-md p-4">
+    <div className="max-w my-20 p-4">
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        <label>
-          趣味:
+        <label htmlFor="hobby" className="my-4 flex justify-between">
+          趣味
           <input
             type="text"
             name="hobby"
             value={formData.hobby ?? ""}
             onChange={handleChange}
             required
-            className="w-full border p-2"
+            className="w-1/2 rounded-xl border border-gray-500 bg-white p-2"
           />
         </label>
 
-        <label>
-          自己紹介:
+        <label htmlFor="introduction" className="my-4 flex items-center justify-between">
+          自己紹介
           <input
             type="text"
             name="introduction"
             value={formData.introduction ?? ""}
             onChange={handleChange}
             required
-            className="w-full border p-2"
+            className="w-1/2 rounded-xl border border-gray-500 bg-white p-2"
           />
         </label>
 
-        <button type="submit" className="rounded bg-blue-500 p-2 text-white" disabled={status === "loading"}>
-          {status === "loading" ? "登録中..." : "登録"}
-        </button>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="mt-15 w-50 rounded bg-blue-500 p-2 text-white"
+            disabled={status === "loading"}
+          >
+            {status === "loading" ? "登録中..." : "登録"}
+          </button>
+        </div>
       </form>
     </div>
   );
