34422e4250efb4fc41bd81ccf4bbf2237b1307e5
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-04 10:54:51 +0900

    fix: sidenav

--- web/src/app/(auth)/settings/layout.tsx
+++ web/src/app/(auth)/settings/layout.tsx
@@ -3,8 +3,12 @@ import SideNav from "@/features/setting/SideNav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <SideNav />
-      {children}
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-64 flex-none">
+        <SideNav />
+      </div>
+      <div className="overflow-y-auto p-12">{children}</div>
+    </div>
     </>
   );
 }
--- web/src/features/setting/SideNav.tsx
+++ web/src/features/setting/SideNav.tsx
@@ -1,30 +1,44 @@
 "use client";
 import Link from "next/link";
 import { useGoogleLogout } from "@/features/auth/functions/logout.ts";
+import { usePathname } from "next/navigation";
 
 export default function SideNav() {
   const { logout } = useGoogleLogout();
+  const pathname = usePathname();
 
   return (
     <>
       <h1 className="font-bold text-3xl">設定画面</h1>
       <div className="m-5">
-        <Link href={"/settings/basic"} className="block">
+        <Link href={"/settings/basic"} className={`block ${
+          pathname === "/settings/basic" ? "font-bold underline" : ""
+        }`}>
           基本情報
         </Link>
-        <Link href={"/settings/university"} className="block">
+        <Link href={"/settings/university"} className={`block ${
+          pathname === "/settings/university" ? "font-bold underline" : ""
+        }`}>
           大学情報
         </Link>
-        <Link href={"/settings/language"} className="block">
+        <Link href={"/settings/language"} className={`block ${
+          pathname === "/settings/language" ? "font-bold underline" : ""
+        }`}>
           言語情報
         </Link>
-        <Link href={"/settings/topic"} className="block">
+        <Link href={"/settings/topic"} className={`block ${
+          pathname === "/settings/topic" ? "font-bold underline" : ""
+        }`}>
           トピック
         </Link>
-        <Link href={"#"} className="block">
+        <Link href={"#"} className={`block ${
+          pathname === "#" ? "font-bold underline" : ""
+        }`}>
           友達
         </Link>
-        <Link href={"#"} className="block">
+        <Link href={"#"} className={`block ${
+          pathname === "#" ? "font-bold underline" : ""
+        }`}>
           その他
         </Link>
         <button type="button" className="btn btn-error m-5" onClick={logout}>
