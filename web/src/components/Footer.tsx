90b3fedad4eb2ab991bfe01a3115c17314d4f2f9
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-05 19:23:20 +0900

    mobile ui: footer

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { MessageSquareText, Settings, Users } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+
+  return (
+    <footer className="flex h-16 w-full items-center justify-around bg-tBlue sm:hidden">
+      <Link href={"/community"}>
+        <Users color="white" size={30} />
+      </Link>
+      <Link href={"/chat"}>
+        <MessageSquareText color="white" size={30} />
+      </Link>
+      <Link href={"/settings"}>
+        <Settings color="white" size={30} />
+      </Link>
+    </footer>
+  );
+}
