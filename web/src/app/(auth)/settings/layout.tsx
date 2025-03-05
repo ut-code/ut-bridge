230a621ac88e270d1519866ed1ee5d2d449f6ab5
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-05 15:39:27 +0900

    mobile ui: /settings

--- web/src/app/(auth)/settings/layout.tsx
+++ web/src/app/(auth)/settings/layout.tsx
@@ -1,15 +1,20 @@
+'use client'
+
 import SideNav from "@/features/setting/SideNav";
 import { UserFormProvider } from "@/features/user/UserFormProvider";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
     <>
       <UserFormProvider>
         <div className="flex h-screen flex-row">
-          <div className="center w-1/4 p-20">
+          <div className={`center w-full sm:block sm:w-1/4 sm:p-20 ${ pathname === "/settings" ? "block" : "hidden"}`}>
             <SideNav />
           </div>
-          <div className="w-2/3 p-12">{children}</div>
+          <div className="sm:w-2/3 sm:p-12">{children}</div>
         </div>
       </UserFormProvider>
     </>
--- web/src/features/setting/SideNav.tsx
+++ web/src/features/setting/SideNav.tsx
@@ -9,51 +9,51 @@ export default function SideNav() {
 
   return (
     <>
-      <h1 className="font-bold text-3xl">設定画面</h1>
-      <div className="m-5 border-gray-300 border-r">
+      <h1 className="hidden font-bold text-3xl sm:block">設定画面</h1>
+      <div className="m-5 flex flex-col gap-0.5 border-gray-300 sm:block sm:gap-0 sm:border-r">
         <Link
           href={"/settings/basic"}
-          className={`my-3 block text-xl ${pathname === "/settings/basic" ? "font-bold underline" : ""}`}
+          className={`block rounded-t-xl bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/basic" ? "font-bold underline" : ""}`}
         >
           基本情報
         </Link>
         <Link
           href={"/settings/university"}
-          className={`my-3 block text-xl ${pathname === "/settings/university" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/university" ? "font-bold underline" : ""}`}
         >
           大学情報
         </Link>
         <Link
           href={"/settings/language"}
-          className={`my-3 block text-xl ${pathname === "/settings/language" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/language" ? "font-bold underline" : ""}`}
         >
           言語情報
         </Link>
         <Link
           href={"/settings/topic"}
-          className={`my-3 block text-xl ${pathname === "/settings/topic" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/topic" ? "font-bold underline" : ""}`}
         >
           トピック
         </Link>
         <Link
           href={"/settings/favorite"}
-          className={`my-3 block text-xl ${pathname === "/settings/favorite" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/favorite" ? "font-bold underline" : ""}`}
         >
           お気に入り
         </Link>
         <Link
           href={"/settings/block"}
-          className={`my-3 block text-xl ${pathname === "/settings/block" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/block" ? "font-bold underline" : ""}`}
         >
           ブロック
         </Link>
         <Link
           href={"/settings/other"}
-          className={`my-3 block text-xl ${pathname === "/settings/other" ? "font-bold underline" : ""}`}
+          className={`block rounded-b-xl bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/other" ? "font-bold underline" : ""}`}
         >
           その他
         </Link>
-        <button type="button" className="btn btn-error my-3" onClick={logout}>
+        <button type="button" className="btn btn-outline btn-error my-12 sm:my-3" onClick={logout}>
           ログアウト
         </button>
       </div>
