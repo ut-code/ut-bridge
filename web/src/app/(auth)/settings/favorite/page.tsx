8390c62134a9ee8916e5ebe1660b65e903b77d3e
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-05 17:22:17 +0900

    mobile ui: favorite page

--- web/src/app/(auth)/settings/favorite/page.tsx
+++ web/src/app/(auth)/settings/favorite/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
       </div>
       <div className="max-w my-10 p-4">
         <h1 className="mb-8 hidden text-xl sm:block">お気に入りユーザー</h1>
-        <ul className="m-5 grid w-full grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+        <ul className="grid w-full grid-cols-1 sm:m-5 sm:grid-cols-1 sm:gap-6 md:grid-cols-1 lg:grid-cols-2">
           {favoriteUsers.length === 0 ? (
             <p>お気に入りユーザーはいません。</p>
           ) : (
