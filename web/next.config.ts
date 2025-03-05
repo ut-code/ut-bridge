08662373c7b7a00db12ee63e423c28365d87981d
aster <137767097+aster-void@users.noreply.github.com>
2025-03-05 19:29:40 +0900

    fix build

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,4 +1,4 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+// import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 import type { NextConfig } from "next";
 
