a5bfbeaf02098dfaa566a8030a141414c4126151
Hiroki Oya <172092787+hiroki078@users.noreply.github.com>
2025-03-05 19:56:46 +0900

    mobile ui: footer (#135)
    * mobile ui: footer
    
    * fix style
    
    * link

--- web/src/app/[locale]/(auth)/layout.tsx
+++ web/src/app/[locale]/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { AuthProvider } from "@/features/auth/providers/AuthProvider";
 import { ChatNotificationProvider } from "@/features/chat/NotificationProvider";
@@ -11,6 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <div className="flex h-full flex-col">
             <Header />
             <main className="flex-1 overflow-y-auto bg-tGray ">{children}</main>
+            <Footer />
           </div>
         </ChatNotificationProvider>
       </UserProvider>
