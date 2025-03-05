c8185e105e1361f4b7a77e1913e4940d47076cf0
aster <137767097+aster-void@users.noreply.github.com>
2025-03-05 18:59:09 +0900

    dev: disable setupdevplatform

--- web/next.config.ts
+++ web/next.config.ts
@@ -12,8 +12,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
+// if (process.env.NODE_ENV === "development") {
+// await setupDevPlatform();
+// }
 
 export default nextConfig;
