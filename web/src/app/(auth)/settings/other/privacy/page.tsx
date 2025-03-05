ba36d2e7917c3f89095c5d3324f17c71277ee440
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-05 17:43:38 +0900

    mobile ui: /settings/other/privacy

--- web/src/app/(auth)/settings/other/privacy/page.tsx
+++ web/src/app/(auth)/settings/other/privacy/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function Page() {
   return (
     <>
+      <div className="flex items-center justify-between border-gray-300 border-b p-4 text-xl sm:hidden">
+        <Link href={"/settings/other"}>
+          <ChevronLeft />
+        </Link>
+        プライバシーポリシー
+        <div className="w-6" />
+      </div>
       <div>
         <h1>プライバシーポリシーページ</h1>
       </div>
