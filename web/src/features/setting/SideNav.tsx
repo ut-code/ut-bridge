1c191f99b7e1a30ad81ba0020e47e117a5de0993
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-05 15:42:52 +0900

    style: sidenav padding

--- web/src/features/setting/SideNav.tsx
+++ web/src/features/setting/SideNav.tsx
@@ -13,43 +13,43 @@ export default function SideNav() {
       <div className="m-5 flex flex-col gap-0.5 border-gray-300 sm:block sm:gap-0 sm:border-r">
         <Link
           href={"/settings/basic"}
-          className={`block rounded-t-xl bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/basic" ? "font-bold underline" : ""}`}
+          className={`block rounded-t-xl bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent sm:pl-4 ${pathname === "/settings/basic" ? "font-bold underline" : ""}`}
         >
           基本情報
         </Link>
         <Link
           href={"/settings/university"}
-          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/university" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent sm:pl-4 ${pathname === "/settings/university" ? "font-bold underline" : ""}`}
         >
           大学情報
         </Link>
         <Link
           href={"/settings/language"}
-          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/language" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent sm:pl-4 ${pathname === "/settings/language" ? "font-bold underline" : ""}`}
         >
           言語情報
         </Link>
         <Link
           href={"/settings/topic"}
-          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/topic" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent sm:pl-4 ${pathname === "/settings/topic" ? "font-bold underline" : ""}`}
         >
           トピック
         </Link>
         <Link
           href={"/settings/favorite"}
-          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/favorite" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent sm:pl-4 ${pathname === "/settings/favorite" ? "font-bold underline" : ""}`}
         >
           お気に入り
         </Link>
         <Link
           href={"/settings/block"}
-          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/block" ? "font-bold underline" : ""}`}
+          className={`block bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent sm:pl-4 ${pathname === "/settings/block" ? "font-bold underline" : ""}`}
         >
           ブロック
         </Link>
         <Link
           href={"/settings/other"}
-          className={`block rounded-b-xl bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent ${pathname === "/settings/other" ? "font-bold underline" : ""}`}
+          className={`block rounded-b-xl bg-neutral-50 p-4 pl-8 text-xl sm:bg-transparent sm:pl-4 ${pathname === "/settings/other" ? "font-bold underline" : ""}`}
         >
           その他
         </Link>
