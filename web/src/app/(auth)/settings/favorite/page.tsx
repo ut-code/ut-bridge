aff817aee20c7d0abecab04f9b86782d99ac7400
hiroki078 <172092787+hiroki078@users.noreply.github.com>
2025-03-05 16:52:59 +0900

    wip: favorite user display page

--- web/src/app/(auth)/settings/favorite/page.tsx
+++ web/src/app/(auth)/settings/favorite/page.tsx
@@ -3,14 +3,23 @@
 import { client } from "@/client";
 import UserCard from "@/features/user/UserCard.tsx";
 import { useUserFormContext } from "@/features/user/UserFormProvider";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function Page() {
   const { favoriteUsers, refetchFavoriteUsers } = useUserFormContext();
 
   return (
     <>
+      <div className="flex items-center justify-between border-gray-300 border-b p-4 text-xl sm:hidden">
+        <Link href={"/settings"}>
+          <ChevronLeft />
+        </Link>
+        お気に入り
+        <div className="w-6" />
+      </div>
       <div className="max-w my-10 p-4">
-        <h1 className="mb-8 text-xl">お気に入りユーザー</h1>
+        <h1 className="mb-8 hidden text-xl sm:block">お気に入りユーザー</h1>
         <ul className="m-5 grid w-full grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
           {favoriteUsers.length === 0 ? (
             <p>お気に入りユーザーはいません。</p>
