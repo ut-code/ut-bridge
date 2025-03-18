13c853876d57519b7b798a302d7bac921c85c607
brdgb <104970845+brdgb@users.noreply.github.com>
2025-03-18 17:26:50 +0900

    ランディングページ(mobile ver)

--- web/src/app/[locale]/page.tsx
+++ web/src/app/[locale]/page.tsx
@@ -11,21 +11,21 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex w-full justify-center">
-        <Image src="/app_sample_ja.png" alt="app sample image" width={500} height={100} />
-        <div className="mr-20 flex flex-col items-center justify-center">
+      <div className="flex w-full flex-col-reverse justify-center xl:flex-row">
+        <Image src="/app_sample_ja.png" alt="app sample image" width={500} height={100} className="m-auto xl:m-0" />
+        <div className="my-10 mr-5 flex flex-col items-center justify-center xl:my-0 xl:mr-20">
           <div className="flex flex-col items-start">
-            <p className="-translate-x-20 my-2 bg-tBlue px-10 py-2 text-7xl text-white shadow-2xl">
+            <p className="-translate-x-5 xl:-translate-x-20 my-1 mt-5 bg-tBlue px-10 py-1 text-5xl text-white shadow-md xl:my-2 xl:mt-2 xl:py-2 xl:text-7xl xl:shadow-2xl">
               {t("catch.upper")}
             </p>
-            <p className="my-2 translate-x-20 bg-tYellow px-10 py-2 text-7xl text-white shadow-2xl">
+            <p className="my-1 translate-x-5 bg-tYellow px-10 py-1 text-5xl text-white shadow-md xl:my-2 xl:translate-x-20 xl:py-2 xl:text-7xl xl:shadow-2xl">
               {t("catch.lower")}
             </p>
           </div>
-          <p className="my-4 text-center text-3xl">{t("catch.message")}</p>
+          <p className="my-4 text-center text-xl xl:text-3xl">{t("catch.message")}</p>
           <Link
             href={"/login"}
-            className="my-4 cursor-pointer rounded-full bg-tBlue px-20 py-3 text-center text-3xl text-white"
+            className="my-2 cursor-pointer rounded-full bg-tBlue px-10 py-1 text-center text-white text-xl xl:my-4 xl:px-20 xl:py-3 xl:text-3xl"
           >
             {t("catch.start")}
           </Link>
