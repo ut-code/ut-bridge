a98d7f36c56a3b46a763e99517262efa5cd3efb5
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-03-21 20:01:24 +0900

    delete notfound

--- web/src/app/[locale]/layout.tsx
+++ web/src/app/[locale]/layout.tsx
@@ -1,8 +1,8 @@
-import { routing } from "@/i18n/routing";
+// import { routing } from "@/i18n/routing";
 import "../../tailwind.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { notFound } from "next/navigation";
+// import { notFound } from "next/navigation";
 export default async function RootLayout({
   children,
   params,
@@ -11,9 +11,9 @@ export default async function RootLayout({
   params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
-  if (!routing.locales.includes(locale as "ja" | "en")) {
-    notFound();
-  }
+  // if (!routing.locales.includes(locale as "ja" | "en")) {
+  //   notFound();
+  // }
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
