2f3768189f338e2d279e99b005f656e4fbeb9baf
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2025-03-25 20:01:04 +0900

    Update page.tsx

--- web/src/app/[locale]/(auth)/users/page.tsx
+++ web/src/app/[locale]/(auth)/users/page.tsx
@@ -6,7 +6,6 @@ import { useAuthContext } from "@/features/auth/providers/AuthProvider";
 import { formatUser } from "@/features/format";
 import { useUserContext } from "@/features/user/userProvider.tsx";
 import type { FlatUser } from "common/zod/schema";
-import { useLocale } from "next-intl";
 import { useLocale, useTranslations } from "next-intl";
 // import {Link} from "@/i18n/navigation.ts";
 import { useRouter, useSearchParams } from "next/navigation";
