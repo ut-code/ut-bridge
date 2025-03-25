264037d3aa6a9862f12ae040c06326950ace7253
brdgb <104970845+brdgb@users.noreply.github.com>
2025-03-25 16:39:24 +0900

    ローディングアイコンを中央に配置

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -1,10 +1,10 @@
-export default function Loading(props: { stage: string }) {
+export default function Loading(_props: { stage: string }) {
   return (
     <>
-      <div className="flex h-full flex-col items-center justify-center">
+      <div className="flex h-screen flex-col items-center justify-center">
         <div className="mr-auto ml-auto max-h-8 w-fit flex-1">
           <span className="loading loading-xl loading-spinner" />
-          {/* loading stage {props.stage} */}
+          {/* loading stage {_props.stage} */}
         </div>
       </div>
     </>
