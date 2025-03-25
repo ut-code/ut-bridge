77fbe2aa17128c4ff5784d049e03439d94e33bb6
brdgb <104970845+brdgb@users.noreply.github.com>
2025-03-25 16:23:58 +0900

    loading stage云々をコメントアウト

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -4,7 +4,7 @@ export default function Loading(props: { stage: string }) {
       <div className="flex h-full flex-col items-center justify-center">
         <div className="mr-auto ml-auto max-h-8 w-fit flex-1">
           <span className="loading loading-xl loading-spinner" />
-          loading stage {props.stage}
+          {/* loading stage {props.stage} */}
         </div>
       </div>
     </>
