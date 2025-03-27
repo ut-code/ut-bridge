d58e117d373cb1cb02243dd41caffb93f21becda
brdgb <104970845+brdgb@users.noreply.github.com>
2025-03-27 22:41:49 +0900

    link

--- web/src/app/[locale]/(auth)/settings/contact/page.tsx
+++ web/src/app/[locale]/(auth)/settings/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <h1 className="font-bold text-2xl text-gray-800 sm:text-3xl dark:text-white">
         {t("setting.other.contact.title")}
       </h1>
-      <a className="text-blue-400" href="https://forms.gle/nL1D4JZ6BgfvsTT56">
+      <a className="link" href="https://forms.gle/nL1D4JZ6BgfvsTT56">
         {t("setting.other.contact.contactForm")}
       </a>
     </div>
