3e04b093913f6d61a588513a3c9396dea72082f8
brdgb <104970845+brdgb@users.noreply.github.com>
2025-03-27 22:45:23 +0900

    delete dark

--- web/src/app/[locale]/(auth)/settings/contact/page.tsx
+++ web/src/app/[locale]/(auth)/settings/contact/page.tsx
@@ -6,9 +6,7 @@ export default function Page() {
   const t = useTranslations();
   return (
     <div>
-      <h1 className="font-bold text-2xl text-gray-800 sm:text-3xl dark:text-white">
-        {t("setting.other.contact.title")}
-      </h1>
+      <h1 className="font-bold text-2xl text-gray-800 sm:text-3xl">{t("setting.other.contact.title")}</h1>
       <a className="link" href="https://forms.gle/nL1D4JZ6BgfvsTT56">
         {t("setting.other.contact.contactForm")}
       </a>
