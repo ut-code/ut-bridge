677ed50046fc6f480dec09087221ac50e328dc2f
brdgb <104970845+brdgb@users.noreply.github.com>
2025-03-27 22:39:58 +0900

    翻訳など

--- web/src/app/[locale]/(auth)/settings/contact/page.tsx
+++ web/src/app/[locale]/(auth)/settings/contact/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 export default function Page() {
+  const t = useTranslations();
   return (
-    <div className="px-4 py-10 sm:px-6 md:px-8 dark:bg-zinc-900">
-      <div className="mx-auto max-w-3xl space-y-8 rounded-2xl border border-gray-200 bg-white p-6 shadow-xl sm:p-8 dark:border-zinc-700 dark:bg-zinc-900">
-        <h1 className="font-bold text-2xl text-gray-800 sm:text-3xl dark:text-white">お問い合わせ</h1>
-        <a href="https://forms.gle/nL1D4JZ6BgfvsTT56">お問い合わせフォーム</a>
-      </div>
+    <div>
+      <h1 className="font-bold text-2xl text-gray-800 sm:text-3xl dark:text-white">
+        {t("setting.other.contact.title")}
+      </h1>
+      <a className="text-blue-400" href="https://forms.gle/nL1D4JZ6BgfvsTT56">
+        {t("setting.other.contact.contactForm")}
+      </a>
     </div>
   );
 }
