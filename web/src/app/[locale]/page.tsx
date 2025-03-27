16aa749b831a2bdafeaa132a52fc362e78734e22
Hideaki HASHIMOTO <104970845+brdgb@users.noreply.github.com>
2025-03-27 12:27:19 +0900

    キャッチコピーが改行しないようにする。ヘッダーもタイトル追加 (#186)

--- web/src/app/[locale]/page.tsx
+++ web/src/app/[locale]/page.tsx
@@ -10,15 +10,15 @@ export default function LandingPage() {
   const t = useTranslations();
   return (
     <>
-      <Header title="" />
+      <Header title="UT-Bridge" />
       <div className="mt-10 flex w-full flex-col-reverse justify-center xl:flex-row">
         <Image src="/app_sample_ja.png" alt="app sample image" width={500} height={100} className="m-auto xl:m-0" />
         <div className="my-10 mr-5 flex flex-col items-center justify-center xl:my-0 xl:mr-20">
           <div className="flex flex-col items-start">
-            <p className="-translate-x-5 xl:-translate-x-20 my-1 mt-5 bg-tBlue px-10 py-1 text-5xl text-white shadow-md xl:my-2 xl:mt-2 xl:py-2 xl:text-7xl xl:shadow-2xl">
+            <p className="-translate-x-5 xl:-translate-x-20 my-1 mt-5 bg-tBlue px-10 py-1 text-[min(10vw,48px)] text-white shadow-md xl:my-2 xl:mt-2 xl:py-2 xl:text-7xl xl:shadow-2xl">
               {t("catch.upper")}
             </p>
-            <p className="my-1 translate-x-5 bg-tYellow px-10 py-1 text-5xl text-white shadow-md xl:my-2 xl:translate-x-20 xl:py-2 xl:text-7xl xl:shadow-2xl">
+            <p className="my-1 translate-x-5 bg-tYellow px-10 py-1 text-[min(10vw,48px)] text-white shadow-md xl:my-2 xl:translate-x-20 xl:py-2 xl:text-7xl xl:shadow-2xl">
               {t("catch.lower")}
             </p>
           </div>
