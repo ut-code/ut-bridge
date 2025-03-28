76182102e6195fa173d76cf1c717d075406c6ea5
brdgb <104970845+brdgb@users.noreply.github.com>
2025-03-28 23:10:50 +0900

    お問い合わせフォームを埋め込む

--- web/src/app/[locale]/(auth)/settings/contact/page.tsx
+++ web/src/app/[locale]/(auth)/settings/contact/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { useTranslations } from "next-intl";
+import { styles } from "../shared-class.ts";
 
 export default function Page() {
   const t = useTranslations();
   return (
     <div>
-      <h1 className="font-bold text-2xl text-gray-800 sm:text-3xl">{t("settings.other.contact.title")}</h1>
-      <a className="link" href="https://forms.gle/nL1D4JZ6BgfvsTT56">
-        {t("settings.other.contact.contactForm")}
-      </a>
+      <h1 className={styles.usersH1}>{t("settings.other.contact.title")}</h1>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScioyR0gN9-UUgsPaePP-EO8oVryRYmdOqqb2aULkc2RZmUkA/viewform?embedded=true"
+        width="640"
+        height="666"
+        className="max-w-full"
+        title="contact form"
+      >
+        読み込んでいます…
+      </iframe>
     </div>
   );
 }
