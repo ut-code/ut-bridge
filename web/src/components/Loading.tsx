e7b2381d8d5399d984c880e94680952b797f5072
aster <137767097+aster-void@users.noreply.github.com>
2025-03-29 20:28:41 +0900

    fixed the app stuck in loading

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -3,8 +3,7 @@ export default function Loading(_props: { stage: string }) {
     <>
       <div className="flex h-screen flex-col items-center justify-center">
         <div className="mr-auto ml-auto max-h-8 w-fit flex-1">
-          <span className="loading loading-xl loading-spinner" />
-          {/* loading stage {_props.stage} */}
+          <span className="loading loading-xl loading-spinner" /> loading stage: {_props.stage}
         </div>
       </div>
     </>
