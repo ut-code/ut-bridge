1a743f426ecb508622dafe85bfa6a614dbd1ba00
aster <137767097+aster-void@users.noreply.github.com>
2025-03-29 20:31:35 +0900

    allow debug mode switch

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -1,9 +1,11 @@
+const debug_mode = false;
 export default function Loading(_props: { stage: string }) {
   return (
     <>
       <div className="flex h-screen flex-col items-center justify-center">
         <div className="mr-auto ml-auto max-h-8 w-fit flex-1">
-          <span className="loading loading-xl loading-spinner" /> loading stage: {_props.stage}
+          <span className="loading loading-xl loading-spinner" />
+          {debug_mode && ` loading stage: ${_props.stage}`}
         </div>
       </div>
     </>
