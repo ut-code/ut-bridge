7ebfbdbd44dd28947f3386f336fe270df7e8ddda
brdgb <104970845+brdgb@users.noreply.github.com>
2025-04-01 22:42:25 +0900

    変数名変更

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -2,4 +2,4 @@
 export const PATHNAME_LANG_PREFIX_PATTERN = /^\/(ja|en)/;
 
 // registration formのstep 1の入力内容をsession storageに保存するkey
-export const STEP_1_DATA_SESSION_STORAGE_KEY = "utBridgeStep1Data";
+export const STEP_1_DATA_SESSION_STORAGE_KEY = "ut_bridge_step_1_data";
