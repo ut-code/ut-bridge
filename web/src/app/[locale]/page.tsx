36c1b81d2a4eb4dfc06f119e55d0b06b682664ab
Hideaki HASHIMOTO <104970845+brdgb@users.noreply.github.com>
2025-04-06 00:50:04 +0900

    /ja向けの画像を差し替え、/en向けの画像を用意、切り替える (#281)

--- web/src/app/[locale]/page.tsx
+++ web/src/app/[locale]/page.tsx
@@ -5,14 +5,22 @@ import LanguageSwitcher from "@/components/LanguageSelectar";
 import { Link } from "@/i18n/navigation.ts";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function LandingPage() {
   const t = useTranslations();
+  const pathname = usePathname();
   return (
     <>
       <Header />
       <div className="mt-10 flex w-full flex-col-reverse justify-center xl:flex-row">
-        <Image src="/app_sample_ja.png" alt="app sample image" width={500} height={100} className="m-auto xl:m-0" />
+        <Image
+          src={pathname.startsWith("/ja") ? "/app_sample_ja.png" : "/app_sample_en.png"}
+          alt="app sample image"
+          width={500}
+          height={100}
+          className="m-auto xl:m-0"
+        />
         <div className="my-10 mr-5 flex flex-col items-center justify-center xl:my-0 xl:mr-20">
           <div className="flex flex-col items-start">
             <p className="-translate-x-5 xl:-translate-x-20 my-1 mt-5 bg-tBlue px-10 py-1 text-[min(10vw,48px)] text-white shadow-md xl:my-2 xl:mt-2 xl:py-2 xl:text-7xl xl:shadow-2xl">
