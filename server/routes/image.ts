89f0a283ade837868b6d4d10cdb15d3adaca4cc6
Hideaki HASHIMOTO <104970845+brdgb@users.noreply.github.com>
2025-04-14 11:06:55 +0900

    画像をpublic accessにすることで消えないようにする (#293)

--- server/routes/image.ts
+++ server/routes/image.ts
@@ -1,4 +1,4 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
@@ -7,14 +7,15 @@ import { z } from "zod";
 // Cloudflare R2 の S3 設定
 const accessKeyId = process.env.CLOUDFLARE_R2_ACCESS_KEY;
 const secretAccessKey = process.env.CLOUDFLARE_R2_SECRET_KEY;
+const userImageDomain = process.env.USER_IMAGE_DOMAIN;
 
-if (!accessKeyId || !secretAccessKey) {
+if (!accessKeyId || !secretAccessKey || !userImageDomain) {
   throw new Error("Missing Cloudflare R2 credentials");
 }
 
 const s3 = new S3Client({
   region: "auto",
-  endpoint: "https://c692d351b94b9f1f32c04143499cba82.r2.cloudflarestorage.com",
+  endpoint: "https://df6c3acd32f66bd1eb95e50607684297.r2.cloudflarestorage.com/ut-bridge-user-image",
   credentials: {
     accessKeyId,
     secretAccessKey,
@@ -24,16 +25,7 @@ const s3 = new S3Client({
 const router = new Hono()
   .get("/get", zValidator("query", z.object({ key: z.string() })), async (c) => {
     const key = c.req.valid("query").key;
-
-    const command = new GetObjectCommand({
-      Bucket: "ut-bridge",
-      Key: key,
-    });
-
-    // 署名付きURLを生成（有効期限 7 day）
-    const url = await getSignedUrl(s3, command, {
-      expiresIn: 60 * 60 * 24 * 7,
-    });
+    const url = `${userImageDomain}/${key}`;
 
     return c.json({ url });
   })
@@ -44,7 +36,7 @@ const router = new Hono()
       s3,
       // TODO(security): limit Content-Length to 10MB
       new PutObjectCommand({
-        Bucket: "ut-bridge",
+        Bucket: "ut-bridge-user-image",
         Key: fileName,
         // Conditions: // [["content-length-range", 0, 10 * 1024 * 1024]], // 10MBまで許可
         // Expires: 600,
