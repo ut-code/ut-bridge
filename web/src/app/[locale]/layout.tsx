1994f8f8edb6a0d9bfd338fa11f7769c8404cefa
aster <137767097+aster-void@users.noreply.github.com>
2025-04-23 12:50:16 +0900

    fuck you next

--- web/src/app/[locale]/layout.tsx
+++ web/src/app/[locale]/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="manifest" href="/manifest.webmanifest" crossorigin="use-credentials" />
+        <link rel="manifest" href="/manifest.webmanifest" crossOrigin="use-credentials" />
         <title>ut-bridge</title>
       </head>
       <body className="flex h-screen flex-col overflow-y-auto">
